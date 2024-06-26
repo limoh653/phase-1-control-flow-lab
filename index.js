a70150ebd17b9ce19019b04eef92d247d741d3fa
limoh653 <limohesbon7@gmail.com>
2024-06-26 14:38:42 +0300

    conditional operators

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if (ride<=400) {
+    return "This one is on me!"; 
+} else if (ride >=400 && ride <=2000) {
+    return "That will be twenty bucks.";
+} else if (ride>2000 && ride<2500) {
+    return "I will gladly take your thirty bucks.";
+} else;{
+    return "No can do.";}
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  // Write your code here! 
+
+  switch (tip) {
+    case 'generous':
+         return 'Thank you so much.';
+    case 'not as generous':
+        return 'Thank you.';
+    default:
+      return  'Bye.'; 
+        }
+      
 }
